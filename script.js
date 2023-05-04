281676ecfe5387677238dca55109be661e70c2ad
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-05-04 12:24:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+// create an array of 5 promises
+        const promises = Array.from({
+                length: 5
+            }, () =>
+            new Promise((resolve) =>
+                setTimeout(() => resolve(Math.random() * 4 + 1), Math.random() * 4000 + 1000)
+            )
+        );
+
+// wait for the first promise to resolve using Promise.any
+        Promise.any(promises).then((result) => {
+
+								   // print the result to the output div
+            document.getElementById('output').textContent = `The first promise resolved with ${result.toFixed(2)} seconds`;
+        });
